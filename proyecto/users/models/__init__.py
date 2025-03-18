621480e94d868ec5231ffdcabc4556bff26922bd
rinndp <alexgualpa012@hotmail.com>
2025-03-18 14:08:48 +0100

    first commit

---
+++ proyecto/users/models/__init__.py
@@ -0,0 +1,2 @@
+from user_model import *
+from role_model import *
---
+++ proyecto/users/models/role_model.py
@@ -0,0 +1,27 @@
+
+from django.db import models
+from django.utils.text import slugify
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, null=False, verbose_name="Nombre del rol", help_text="Solo ADMIN y CLIENT")
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        db_table = "roles"
+        verbose_name = "Rol"
+        verbose_name_plural = "Roles"
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = slugify(self.name)
+            cont = 1
+            while Role.objects.filter(slug=slug).exists():
+                sluf = f"{slug}-{cont}"
+                cont += 1
+
+
+        super().save(*args, **kwargs)
+
+
